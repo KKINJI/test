9ac7a16cdb6304d92773c6af308504dbce207259
kkinji <thuknio@hanmail.net>
2021-10-08 15:13:52 +0900

    커밋

--- test/call.js
+++ test/call.js
@@ -1,4 +1,26 @@
- export let data = [ '1', '2', '3', '4'
-			
-		];
-		
+export default [
+	{
+	  "question": "저는 몇살 일까요?",
+	  "anwser": "1993년에 태어난 28살입니다.",
+	},
+	{
+	  "question": "저의 혈액형은 무엇일까요?",
+	  "anwser": "O형 입니다.",
+	},
+	{
+	  "question": "저는 어디에 살까요?",
+	  "anwser": "강남구 대치동에 살고 있습니다.",
+	},
+	{
+	  "question": "저의 고향은 어디일까요?",
+	  "anwser": "부산입니다.",
+	},
+	{
+	  "question": "저의 MBTI 유형은 무엇일까요?",
+	  "anwser": "ENTJ 입니다.",
+	},
+	{
+	  "question": "저의 취미는 무엇일까요?",
+	  "anwser": "요즘은 JS관련 구글링..? ^^.",
+	}
+  ];
---
+++ test/test.js
@@ -0,0 +1,3 @@
+import data from "./call.js"
+
+console.log(data);
