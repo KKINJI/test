3c1a1ecf518916c3a2cb5a01d1a151a6796b1699
kkinji <thuknio@hanmail.net>
2021-10-08 15:03:17 +0900

    commit

--- test/call.js
+++ test/call.js
@@ -1,8 +1,4 @@
- export let data = [
-			[36.35613994325377, 127.33330258912929,'<div style="padding:5px;"> 1</div>','<div class ="label">카카오!</div>'],
-            [36.35867388691857, 127.33805215959592,'<div style="padding:5px;"> 2</div>','<div class ="label">카카오!</div>'],
-            [36.35805726933201, 127.34241559363039,'<div style="padding:5px;"> 3</div>','<div class ="label">카카오!</div>'],
-			[36.35948793297557, 127.33687484885225,'<div style="padding:5px;"> 4</div>','<div style="padding:0 5px;background:#fff;">카카오!</div>'],
-			[36.364399230016616, 127.33465458334764,'<div style="padding:5px;"> 5</div>','<div class ="label"><span class="left"></span><span class="center">카카오!</span><span class="right"></span></div>']
+ export let data = [ '1', '2', '3', '4'
+			
 		];
 		
